a4e81c5e26e6fb54aa1c3e76c8aae5357711d736
Alex Bessa <alexbessa.dev@gmail.com>
2026-07-16 21:05:15 -0300

    feat: adicionar interface para recurso de usuarios

--- server.mjs
+++ server.mjs
@@ -1,5 +1,26 @@
 import http from 'node:http';
 
+const users = [{
+    name: 'Alex Bessa',
+    age: 30,
+    status: true
+}, {
+    name: 'Valdiana Bessa',
+    age: 31,
+    status: true
+}, {
+    name: 'Ana Bessa',
+    age: 0,
+    status: false
+}];
+
 http.createServer((request, response) => {
-    response.end('Hello Client!');
+    if (request.url === '/users') {
+        response.writeHead(200, { 'content-type': 'application/json' });
+        response.end(JSON.stringify(users));
+    } else {
+        response.writeHead(404, { 'content-type': 'application/json' });
+        response.end(JSON.stringify({ message: 'Página não encontrada.' }));
+    }
 }).listen(3000);
+    
