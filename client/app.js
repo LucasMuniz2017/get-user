11c3c425ee9ac0ad6b5c94a79d2e2621806d5251
Lucas Gama <lucasgmuniz.2017@gmail.com>
2026-08-02 09:28:04 -0300

    feat: adiciona busca de usuário por id com exibição dos dados

--- client/app.js
+++ client/app.js
@@ -1,5 +1,6 @@
 const button = document.querySelector('button');
 const input = document.querySelector('input');
+const result = document.querySelector('p')
 
 const resource = 'users';
 
@@ -13,11 +14,14 @@ async function getUsers() {
     const user = data.find(user => user.id === id);
 
     if (!user) {
+        result.textContent = `Usuário de id ${id} não encontrado.`;
         console.log(`Usuário de id ${id} não encontrado.`);
         return;
     }
 
+    result.textContent = `id de usuário: ${user.id} Nome: ${user.name} Idade: ${user.age} Status de usuário: ${user.status}`
     console.log(user);
+    
 }
 
 button.addEventListener('click', getUsers);
